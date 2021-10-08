bf11a251303bfb2a46e7e32d41a385b32507a351
G-hoon <new0126@naver.com>
2021-10-08 17:23:54 +0900

    feat(#5): [개발] nickname editor 화면 및 팔로우 목록 출력 화면 구현
    - nickname editor screen 구현
    - follow list screen 구현

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,15 @@
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+    const followingList = [{ 'nickname': 'qwer'}, { 'nickname': 'rrrr'}, { 'nickname': 'tttt'}];
+    const followerList = [{ 'nickname': 'asdf'}, { 'nickname': 'ssss'}, { 'nickname': 'zzzzz'}];
     return (
         <AppLayout>
-            <div>내 프로필</div>
+            <NicknameEditForm />
+            <FollowList header="팔로잉 목록" data={followingList} />
+            <FollowList header="팔로워 목록" data={followerList} />
         </AppLayout>
     );
 }
