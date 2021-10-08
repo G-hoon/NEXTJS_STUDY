40f505b98af15d3c441105fd23f8957423e23095
G-hoon <new0126@naver.com>
2021-10-08 18:15:37 +0900

    feat(#6): [개발] 커스텀 훅 구현 및 회원가입 페이지 구현
    - custom 훅을 통한 코드 최적화
    - 회원가입 페이지 및 벨리데이션 구현

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,8 +1,9 @@
-import { useState, useCallback, useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
+import useInput from "../hooks/useInput";
 
 // div 태그이면서, css 가 적용된 button wrapper component => 렌더링 최적화 용도
 // re-render 할 때마다, div 태그 다시 생성하지 않기 위함 
@@ -18,17 +19,8 @@ const FormWrapper = styled(Form)`
 // 함수형 컴포넌트에서 re-rendering이 될 경우에 함수 내에 있는 모든 코드가 처음부터 끝까지 다시 시작된다.
 // useCallback이랑 useMemo의 배열 값이 변화하지 않을 경우, 바뀐 것이 없다고 체크하여 다음 코드로 이동 return (...) 에서 바뀐 부분만 다시 렌더링 
 const LoginForm = ({ setIsLoggedIn }) => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
-
-    // useCallback: 함수를 캐싱하기 위한 용도
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    }, []);
-
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value);
-    }, []);
+    const [id, onChangeId] = useInput('');
+    const [password, onChangePassword] = useInput('');
 
     // useMemo: 값을 캐싱 / re-rendering 최적화. => re-rendering을 방지하는 방법은 styled-component와 useMemo 두가지 방법이 있다.
     const style = useMemo(() => ({ marginTop: 10}), [])
